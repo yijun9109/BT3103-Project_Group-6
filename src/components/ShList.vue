c3b791c237c2f314da1b412629025b868e009644
yijun9109 <72440337+yijun9109@users.noreply.github.com>
2021-10-31 09:50:41 +0800

    Update ShList.vue
    modified save

--- src/components/ShList.vue
+++ src/components/ShList.vue
@@ -1,32 +1,12 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    sort-by="quant"
-    class="elevation-1"
-  >
+  <v-data-table :headers="headers" :items="items" sort-by="quant" class="elevation-1">
     <template v-slot:top>
-      <v-toolbar
-        flat
-      >
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
+      <v-toolbar flat>
+        <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-        >
+        <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
               Add Item
             </v-btn>
           </template>
@@ -38,39 +18,18 @@
             <v-card-text>
               <v-container>
                 <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="ITEM NAME"
-                      id="name"
-                    >
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="ITEM NAME" id="name">
                     </v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.quant"
-                      label="Item Quantity"
-                      id="IQuant"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.quant" label="Item Quantity" id="IQuant">
+                    </v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.current"
-                      label="Current Quantity"
-                      id="CQuant"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.current" label="Current Quantity" id="CQuant">
+
+                    </v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
@@ -95,6 +54,50 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
+
+
+        <v-dialog v-model="dialogEdit" max-width="500px">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Edit Item</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="ITEM NAME" id="name1">
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.quant" label="Item Quantity" id="IQuant1">
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.current" label="Current Quantity" id="CQuant1">
+
+                    </v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">
+                Cancel
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="save">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+
+
+
+
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
@@ -107,7 +110,7 @@
           </v-card>
         </v-dialog>
       </v-toolbar>
-    </template>
+    </template> 
     <template v-slot:item.actions="{ item }">
       <v-icon
         small
@@ -135,13 +138,15 @@
 </template>
 
 <script>
-import {  deleteDoc, getDocs, collection, setDoc, doc, getFirestore } from 'firebase/firestore'
+import {  deleteDoc, getDocs, collection, setDoc, doc, getFirestore, updateDoc, } from 'firebase/firestore'
 import firebaseApp from '../firebase'
+
 
 const db = getFirestore(firebaseApp)
   export default {
     data: () => ({
       dialog: false,
+      dialogEdit: false,
       dialogDelete: false,
       headers: [
         {
@@ -169,11 +174,14 @@ const db = getFirestore(firebaseApp)
     }),
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? 'New Item' : 'Edit'
       },
     },
     watch: {
       dialog (val) {
+        val || this.close()
+      },
+      dialogEdit (val) {
         val || this.close()
       },
       dialogDelete (val) {
@@ -187,35 +195,19 @@ const db = getFirestore(firebaseApp)
         let data = docs.data()
         this.items.push({
             name: data.name,
-            quant: data.cquant,
-            current: data.iquant,
+            quant: data.iquant,
+            current: data.cquant,
           })
       })
     },
     methods: {
       initialize () {
-        this.items = [
-          {
-            name: 'Frozen Yogurt',
-            quant: 159,
-            current: 6.0,
-          },
-          {
-            name: 'Ice cream sandwich',
-            quant: 237,
-            current: 9.0,
-          },
-          {
-            name: 'Eclair',
-            quant: 262,
-            current: 16.0,
-          }
-        ]
+        this.items = []
       },
-      async editItem (item) {
+      editItem (item) {
         this.editedIndex = this.items.indexOf(item)
         this.editedItem = Object.assign({}, item)
-        this.dialog = true
+        this.dialogEdit = true
       },
       async deleteItem (item) {
         this.editedIndex = this.items.indexOf(item)
@@ -231,6 +223,7 @@ const db = getFirestore(firebaseApp)
       },
       close () {
         this.dialog = false
+        this.dialogEdit = false
         this.$nextTick(() => {
           this.editedItem = Object.assign({}, this.defaultItem)
           this.editedIndex = -1
@@ -244,25 +237,37 @@ const db = getFirestore(firebaseApp)
         })
       },
       async save () {
+
         if (this.editedIndex > -1) {
           
           Object.assign(this.items[this.editedIndex], this.editedItem)
+          var name1 = document.getElementById("name1").value
+          var iquant1 = document.getElementById("IQuant1").value
+          var cquant1 = document.getElementById("CQuant1").value
+          var i = this.editedItem.name
+          const docRef = doc(db, "Shopping List", i);
+          if (i == name1) {
+            await updateDoc(docRef, {
+              iquant: iquant1,
+              cquant: cquant1
+            })
+          }
           } else {
           this.items.push(this.editedItem)
-
           var name = document.getElementById("name").value
           var iquant = document.getElementById("IQuant").value
           var cquant = document.getElementById("CQuant").value
           const docRef = await setDoc(doc(db, "Shopping List", name), {
           name: name, iquant: iquant, cquant: cquant
           })
-
           const SList = collection(db, "Shopping List");
           const que = await getDocs(SList)
           
           que.forEach((docs) => {
                 let data = docs.data()
                 name += data.name;
+                iquant += data.iquant;
+                cquant += data.cquant;
           })
           console.log(docRef)
           this.$emit("added")
@@ -271,4 +276,5 @@ const db = getFirestore(firebaseApp)
       },
     }
   }
+
 </script>
